7c2df1a2d5b23f2a4d5002606aa7dfc7b5e89d17
sarapekaloki <78003706+sarapekaloki@users.noreply.github.com>
2021-09-25 19:30:09 -0700

    Add files via upload

---
+++ taquero.py
@@ -0,0 +1,20 @@
+class Taquero():
+    def __init__(self,name , carne1, queue1, queue2, carne2=None,):
+        self.name = name
+        self.carne1 = carne1
+        self.carne2 = carne2
+        self.tacosCount = 0
+        self.tortillaCount = 0
+        self.salsaCount = 0
+        self.cebollaCount = 0
+        self.cilantroCount = 0
+        self.aguacateCount = 0
+        self.queue1 = queue1
+        self.queue2 = queue2
+        self.needsBreak = 0
+
+
+
+
+
+
