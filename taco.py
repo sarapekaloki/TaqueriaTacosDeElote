3bf15bb94553a6fac0c9e2dc2a00d0e8049fa481
AngelAlmaraz <64762942+AngelAlmaraz@users.noreply.github.com>
2021-09-25 22:05:14 -0500

    Update taco.py

--- taco.py
+++ taco.py
@@ -1,9 +1,9 @@
 class Taco():
-    def __init__(self, carne,quesadilla, ingredientes=None):   
+    def __init__(self, carne,isQuesadilla, ingredientes=None):   
         self.carne = carne
         self.ingredientes=ingredientes
         self.finishTime=1
-        self.isQuesadilla=False
+        self.isQuesadilla=isQuesadilla
         
     def setTime(self):
         for ingrediente in range(len(self.ingredientes)):
