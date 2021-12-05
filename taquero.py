b4d058352eba5c305b35c8d29a8387b9413b8354
sarapekaloki <78003706+sarapekaloki@users.noreply.github.com>
2021-12-05 14:00:38 -0800

    Update taquero.py

--- taquero.py
+++ taquero.py
@@ -1,9 +1,8 @@
 from queue_functions import *
 from threading import Thread
 from queue import Queue
-
-
 # from Mesero import agregar_ordenes
+
 
 
 class Taquero():
@@ -12,9 +11,11 @@ class Taquero():
         self.queue_2 = queue2
         self.tipos = tipos
         self.fillings = {"salsa": 150, "guacamole": 100,"cilantro": 200,"cebolla": 200}
+        self.tortillas = 50
         self.rest =100  # 3 s / 100
         self.fan =60  # 6 s / 60
         self.using_queue_1 = True
+
 
     def check_rest(self, rest_time):
         print(f"Tiempo restante al descanso = {self.rest}\n")
@@ -44,10 +45,12 @@ class Taquero():
         elif not self.using_queue_1 and not self.queue_2.empty():
             queue = self.queue_2
             num_queue = '2'
-
-        print("\n\nQueue de {0} que se esta usando: {1} ".format(self.tipos ,num_queue))
+        else:
+            queue = self.queue_1
+            num_queue = '1'
 
         if not queue.empty():
+            print("\n\nQueue de {0} que se esta usando: {1} ".format(self.tipos, num_queue))
             orden = queue.get()
             orden['Answer']['start-time']= str(datetime.now())
             for batch in orden['orden']:
@@ -62,25 +65,34 @@ class Taquero():
                         fan_thread.start()
                         orden['Answer']['steps'].append({"state":"running","action":"Doing tacos","part-id":part_id})
                         spent_fillings = int(batch['quantity'])
-                        for ingredient in batch['ingredients']:
-                            sleepTime+=0.5*batch['quantity']
-                            self.fillings[ingredient] -= spent_fillings
-                        sleepTime += batch['quantity']
-                        batch['quantity'] = 0
-                        time.sleep(sleepTime)
-                        batch['status'] = 'closed'
-                        orden['Answer']['steps'].append({"state": "Done", "action": "Batch completed", "part-id": part_id})
-                        if self.using_queue_1:
-                            self.using_queue_1 = False
 
-                        print("Fillings: ", self.fillings)
-                        print(f"Batch despues: {batch}")
-                        print('\t\t >>> Batch terminado')
+                        if self.tortillas<=5:
+                            print("Esperando tortillas")
+                        else:
+                            for ingredient in batch['ingredients']:
+                                if self.fillings[ingredient]<=5:
+                                    print("Esperando ingredientes")
+                                sleepTime+=0.5*batch['quantity']
+                                self.fillings[ingredient] -= spent_fillings
+                            if batch["type"]!= "quesataco":
+                                sleepTime += batch['quantity']
+                            self.tortillas -= batch['quantity']
+                            batch['quantity'] = 0
+                            time.sleep(sleepTime)
+                            batch['status'] = 'closed'
+                            orden['Answer']['steps'].append({"state": "Done", "action": "Batch completed", "part-id": part_id})
+                            if self.using_queue_1:
+                                self.using_queue_1 = False
+
+                            print("Fillings: ", self.fillings)
+                            print(f"Batch despues: {batch}")
+                            print('\t\t >>> Batch terminado')
 
                         # Se regresa al objeto, y la orden que se quiere enviar a otro cocinero. El objeto aqui se regresa porque se necesita
                         # referencia a el, ya que se va a excluir de la lista en la funcion de agregar_ordenes en dum.py. Esto solo se envia cuando
                         # se termino el batch. Aun falta la funcionalidad de que va a pasar cuando la orden este completamente terminada jajaja salu2
-                        return orden
+                        # agregar_ordenes(taqueros,quesadillero,orden)
+                            return orden
 
                     elif batch['quantity'] > 5:
                         sleepTime=0
@@ -90,9 +102,11 @@ class Taquero():
                         for ingredient in batch['ingredients']:
                             self.fillings[ingredient] -= 5
                             sleepTime+=0.5*5
-                        sleepTime+=5
+                        if batch["type"]!="quesataco":
+                            sleepTime+=5
                         batch['quantity'] -= 5
-                        time.sleep(5)
+                        self.tortillas -=5
+                        time.sleep(sleepTime)
 
                         orden['Answer']['steps'].append({"state": "Running", "action": "Doing tacos", "part-id": part_id})
                         self.queue_2.put(orden)
@@ -102,7 +116,6 @@ class Taquero():
                         print(f"Batch despues: {batch}")
                         return
         else:
-            print("El queue esta vacio alaverga")
-
             # Agregar descanso qui vacio
+            print("Queue vacio")
             return
