d8f3b45710995ba0fca8f90d9b4855ae35fcddf9
sarapekaloki <78003706+sarapekaloki@users.noreply.github.com>
2021-12-05 16:27:59 -0800

    Update chalan.py

--- chalan.py
+++ chalan.py
@@ -19,28 +19,22 @@ class Chalan(Thread):
             if i == filling:
                 if filling == "cilantro" or filling == "cebolla":
                     #     queremos que pasen 10 segundos, pero sin parartodo el codigo, solo el thread, por eso usamos nuesto metodo wait
-                    print(f"{taquero.name} se ha quedado sin {filling}")
+
                     taquero.wait(10)
-                    print(f"\t\t >>> Se ha rellendo -> {filling} de {taquero}")
+
                     taquero.fillings[filling] = 200
 
                 # llenamos SALSA
                 elif filling == "salsa":
-                    print(f"{taquero.name} se ha quedado sin {filling}")
                     taquero.wait(15)
-                    print(f"\t\t >>> Se ha rellenau el filling ->{filling} de {taquero}")
                     taquero.fillings[filling] = 150
 
                 # llenamos GUACAMOLE
                 elif filling == "guacamole":
-                    print(f"{taquero.name} se ha quedado sin {filling}")
                     taquero.wait(20)
-                    print(f"\t\t >>> Se ha rellenau el filling ->{filling} de {taquero}")
                     taquero.fillings[filling] = 100
 
                 # Lo unico mas que queda por rellenar son las TORTILLAS
                 else:
-                    print(f"{taquero.name} se ha quedado sin {filling}")
                     taquero.wait(5)
-                    print(f"\t\t >>> Se ha rellenau el filling ->{filling} de {taquero}")
                     taquero.fillings[filling] = 50
